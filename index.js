da84226053028fc177df17d784e4496e1a4e87ea
Joan Torres <joan.torres@upr.edu>
2015-10-12 15:08:41 -0700

    Integrate sockets

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static(__dirname + '/public'));
+
+var users = 0;
+io.on('connection', function(socket){
+  console.log(++users + " challengers online!");
+
+
+  socket.on('disconnect', function(){
+    users--;
+  })
+
+  socket.on('move made', function(move){
+    socket.broadcast.emit('my turn', move);
+  });
+});
 
 http.listen(3000, function(){
   console.log('ready for action on some-badass-host:3000');
